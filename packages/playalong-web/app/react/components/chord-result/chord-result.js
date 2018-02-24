dd3e6b5e7704290360ab4b148bd3b848e0446c38
atardadi <datar@medallia.com>
2018-02-24 16:12:14 +0200

    build is passing

--- packages/playalong-web/app/react/components/chord-result/chord-result.js
+++ packages/playalong-web/app/react/components/chord-result/chord-result.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { func, object } from 'prop-types';
 import styled from 'styled-components';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import PlyImage from 'playalong-components/components/Image';
-import Rating from 'playalong-components/components/Rating';
+import PlyImage from 'playalong-components/lib/components/Image';
+import Rating from 'playalong-components/lib/components/Rating';
 
 import THEME from '../../helpers/theme';
 
