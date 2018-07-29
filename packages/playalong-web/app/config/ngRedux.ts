2eac77e49916b9540afd0f740fbd39ced16b121f
Dadi Atar <atardadi@gmail.com>
2018-07-29 16:58:49 +0300

    fix redux dev tools bug

--- packages/playalong-web/app/config/ngRedux.ts
+++ packages/playalong-web/app/config/ngRedux.ts
@@ -1,15 +1,19 @@
 import { chordSearchReducer } from '../redux/reducers/chord-search';
 import { singleChordReducer } from '../redux/reducers/single-chord';
 
+const __REDUX_DEVTOOLS_EXTENSION_ = window.__REDUX_DEVTOOLS_EXTENSION__ ?
+	[window.__REDUX_DEVTOOLS_EXTENSION__()] : null;
+
+
 ngReduxConfig.$inject = ['$ngReduxProvider'];
 function ngReduxConfig($ngReduxProvider) {
 	$ngReduxProvider.createStoreWith({
 		chordSearch: chordSearchReducer,
 		singleChord: singleChordReducer,
 
-	}, 
-	[], 
-	[window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()]);
+	},
+	[],
+	__REDUX_DEVTOOLS_EXTENSION_);
 }
 
 export default ngReduxConfig;
