aaadda43ab7459a8dc7074279647e1d638948eb0
Dadi Atar <atardadi@gmail.com>
2018-08-10 17:41:13 +0300

    stabilize build

--- packages/playalong-web/app/utils/angular-polyfills.ts
+++ packages/playalong-web/app/utils/angular-polyfills.ts
@@ -1,10 +1,3 @@
-export default function angularPolyfill() {
-	if (typeof window.angular !== undefined) {
-		if (typeof window.angular.lowercase !== 'function') {
-			window.angular.lowercase = text => {
-				return typeof text === 'string' ? text.toLowerCase() : text;
-			};
-		}
-	}
-
-}
+(window as any).angular.lowercase = text => {
+	return typeof text === 'string' ? text.toLowerCase() : text;
+};
