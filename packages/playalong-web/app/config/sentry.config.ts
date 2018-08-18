afcf2645a1230c777fa82171a8d3715ddccf9557
Dadi Atar <atardadi@gmail.com>
2018-08-18 16:28:46 +0300

    removed sentry

--- packages/playalong-web/app/config/sentry.config.ts
+++
@@ -1,6 +0,0 @@
-// const Raven = require('raven-js');
-
-// Raven
-//   .config('https://23405297a5c441f29a2b51c640789371@sentry.io/293400')
-//   .addPlugin(require('raven-js/plugins/angular'), angular)
-//   .install();
